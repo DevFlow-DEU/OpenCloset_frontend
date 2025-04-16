4f6a71a14409fa01568b6d00d998faff2f41f6f5
Jihong Kim <34394165+jihongeek@users.noreply.github.com>
2025-04-16 15:18:52 +0900

    Feat: 메인페이지 마크업 및 상세 페이지 컴포넌트 구현 (#5)
    * Chore: 페이지 `title` 수정
    
    * Chore: 리액트 라우터 설치 및 설정
    
    * Chore: svg를 리액트 컴포넌트로 가져오기 위한 `vite-plugin-svgr` 설치 및 설정
    
    * Feat: 네비게이션 바 및 헤더 UI 마크업 구현
    
    * Chore: 아이콘 추가 (#3)
    
    네비바 아이콘, 카테고리, 잡 아이콘 추가
    
    * Feat: 세부 페이지 초기 완성 (#4)
    
    * icon 추가
    
    네비바 아이콘, 카테고리, 잡 아이콘 추가
    
    * 상세 페이지 jsx css 추가
    
    상세 페이지 jsx css 추가 (초기)
    
    * Feat: 홈 화면 카테고리 마크업 제작
    
    * Feat: 매물 리스트 UI 마크업 구현
    
    * Chore: 대여 상품을 위한 의류 이미지 추가
    
    * Feat: 대여 상품 추가 버튼 마크업 추가
    
    ---------
    
    Co-authored-by: yooningu <yig0717@naver.com>

--- src/routes/pages/Home.jsx
+++ src/routes/pages/Home.jsx
@@ -1,3 +1,252 @@
+import { Link } from 'react-router-dom';
+import styles from './Home.module.css';
+import Logo from '../../assets/logo.svg?react';
+import HamburgerIcon from '../../assets/hamburger.svg?react';
+import SearchIcon from '../../assets/search.svg?react';
+import SaveIcon from '../../assets/save.svg?react';
+import MapIcon from '../../assets/map.svg?react';
+import ProfileIcon from '../../assets/profile.svg?react';
+import HomeIcon from '../../assets/home.svg?react';
+import ChatIcon from '../../assets/chat.svg?react';
+import TopsIcon from '../../assets/icon/Category/Tops.svg?react';
+import PantsIcon from '../../assets/icon/Category/Pants.svg?react';
+import OuterIcon from '../../assets/icon/Category/Outer.svg?react';
+import BagIcon from '../../assets/icon/Category/Bag.svg?react';
+import JewelryIcon from '../../assets/icon/Category/Jewelry.svg?react';
+import OnePieceIcon from '../../assets/icon/Category/OnePiece.svg?react';
+import ShoesIcon from '../../assets/icon/Category/Shoes.svg?react';
+import AccessoryIcon from '../../assets/icon/Category/Accessory.svg?react';
+import LikeGrayHeartIcon from '../../assets/icon/Like_gray_heart.svg?react';
+import LikeRedHeartIcon from '../../assets/icon/Like_red_heart.svg?react';
+import AddProductIcon from '../../assets/icon/Add_product.svg?react';
 export default function Home() {
-  return <>Home</>;
-}
+  return (
+    <>
+      <header>
+        <Link className='hamburger-button'>
+          <HamburgerIcon height={16.92} width={22.38} />
+        </Link>
+        <Logo height={20} width={118} />
+        <Link className='search-button'>
+          <SearchIcon height={16.92} width={22.38} />
+        </Link>
+      </header>
+      <div className={styles.container}>
+        <div className='slider'>
+          <img src='/광고창.png' alt='광고창' width='100%' />
+        </div>
+        <div className='category-list'>
+          <div className={styles['category-header']}>Categories</div>
+          <div className={styles['category-container']}>
+            <div className={styles['category-item']}>
+              <Link>
+                <div className={styles['category-icon']}>
+                  <TopsIcon width='39.26px' height='39.26px' />
+                </div>
+                상의
+              </Link>
+            </div>
+            <div className={styles['category-item']}>
+              <Link>
+                <div className={styles['category-icon']}>
+                  <PantsIcon width='39.26px' height='39.26px' />
+                </div>
+                하의
+              </Link>
+            </div>
+            <div className={styles['category-item']}>
+              <Link>
+                <div className={styles['category-icon']}>
+                  <OuterIcon width='39.26px' height='39.26px' />
+                </div>
+                아우터
+              </Link>
+            </div>
+            <div className={styles['category-item']}>
+              <Link>
+                <div className={styles['category-icon']}>
+                  <JewelryIcon width='39.26px' height='39.26px' />
+                </div>
+                주얼리
+              </Link>
+            </div>
+            <div className={styles['category-item']}>
+              <Link>
+                <div className={styles['category-icon']}>
+                  <OnePieceIcon width='39.26px' height='39.26px' />
+                </div>
+                원피스
+              </Link>
+            </div>
+            <div className={styles['category-item']}>
+              <Link>
+                <div className={styles['category-icon']}>
+                  <ShoesIcon width='39.26px' height='39.26px' />
+                </div>
+                신발
+              </Link>
+            </div>
+            <div className={styles['category-item']}>
+              <Link>
+                <div className={styles['category-icon']}>
+                  <BagIcon width='39.26px' height='39.26px' />
+                </div>
+                가방
+              </Link>
+            </div>
+            <div className={styles['category-item']}>
+              <Link>
+                <div className={styles['category-icon']}>
+                  <AccessoryIcon width='39.26px' height='39.26px' />
+                </div>
+                악세서리
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className='item-list'>
+          <h2 className={styles['item-list-header']}>
+            오늘의 <br /> 추천 상품
+          </h2>
+          <div className={styles['item-container']}>
+            <div className={styles['item']}>
+              <Link>
+                <button className={styles['save-button']}>
+                  <LikeRedHeartIcon width={'16.06px'} height={'14.34px'} />
+                </button>
+                <div className={styles['item-thumbnail']}>
+                  <img
+                    src='/hoodie.png'
+                    alt='hoodie'
+                    width='170.13px'
+                    height='169.56px'
+                  />
+                </div>
+                <div className={styles['item-description-container']}>
+                  <div className={styles['item-name']}>
+                    Script Logo Heavyweigh...
+                  </div>
+                  <div>
+                    <span className={styles['item-cost']}>3,700원</span>
+                    <span className={styles['item-period']}> / 3일</span>
+                  </div>
+                  <div className={styles['item-location']}>중앙동</div>
+                </div>
+              </Link>
+            </div>
+            <div className={styles['item']}>
+              <Link>
+                <button className={styles['save-button']}>
+                  <LikeGrayHeartIcon width={'16.06px'} height={'14.34px'} />
+                </button>
+                <div className={styles['item-thumbnail']}>
+                  <img
+                    src='/windbreaker.png'
+                    alt='windbreaker'
+                    width='170.13px'
+                    height='169.56px'
+                  />
+                </div>
+                <div className={styles['item-description-container']}>
+                  <div className={styles['item-name']}>
+                    Script Logo Heavyweigh...
+                  </div>
+                  <div>
+                    <span className={styles['item-cost']}>3,700원</span>
+                    <span className={styles['item-period']}> / 3일</span>
+                  </div>
+                  <div className={styles['item-location']}>중앙동</div>
+                </div>
+              </Link>
+            </div>
+            <div className={styles['item']}>
+              <Link>
+                <button className={styles['save-button']}>
+                  <LikeGrayHeartIcon width={'16.06px'} height={'14.34px'} />
+                </button>
+                <div className={styles['item-thumbnail']}>
+                  <img
+                    src='/pants.png'
+                    alt='pants'
+                    width='170.13px'
+                    height='169.56px'
+                  />
+                </div>
+                <div className={styles['item-description-container']}>
+                  <div className={styles['item-name']}>
+                    Script Logo Heavyweigh...
+                  </div>
+                  <div>
+                    <span className={styles['item-cost']}>3,700원</span>
+                    <span className={styles['item-period']}> / 3일</span>
+                  </div>
+                  <div className={styles['item-location']}>중앙동</div>
+                </div>
+              </Link>
+            </div>
+            <div className={styles['item']}>
+              <Link>
+                <button className={styles['save-button']}>
+                  <LikeGrayHeartIcon width={'16.06px'} height={'14.34px'} />
+                </button>
+                <div className={styles['item-thumbnail']}>
+                  <img
+                    src='/pants1.png'
+                    alt='pants1'
+                    width='170.13px'
+                    height='169.56px'
+                  />
+                </div>
+                <div className={styles['item-description-container']}>
+                  <div className={styles['item-name']}>
+                    Script Logo Heavyweigh...
+                  </div>
+                  <div>
+                    <span className={styles['item-cost']}>3,700원</span>
+                    <span className={styles['item-period']}> / 3일</span>
+                  </div>
+                  <div className={styles['item-location']}>중앙동</div>
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <Link className={styles['add-button']}>
+          <AddProductIcon />
+        </Link>
+      </div>
+      <div className={styles['nav-bar']}>
+        <Link>
+          <div>
+            <MapIcon width={20.66} height={20.66} />
+          </div>
+          지도
+        </Link>
+
+        <Link>
+          <div>
+            <ChatIcon width={20.66} height={20.66} />
+          </div>
+          채팅
+        </Link>
+        <Link>
+          <div>
+            <HomeIcon width={20.66} height={20.66} />
+          </div>
+          홈
+        </Link>
+        <Link>
+          <div>
+            <SaveIcon width={20.66} height={20.66} />
+          </div>
+          찜
+        </Link>
+        <Link>
+          <div>
+            <ProfileIcon width={20.66} height={20.66} />
+          </div>
+          내 정보
+        </Link>
+      </div>
+    </>
+  );
