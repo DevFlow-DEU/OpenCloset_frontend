11b37c4aa25422ec4b361d1c72eab1f3de1d7c46
jihongeek <jihongeek@kakao.com>
2025-04-18 12:49:14 +0900

    Chore: 검색 페이지 마크업 완성

--- src/routes/pages/Search.jsx
+++ src/routes/pages/Search.jsx
@@ -12,7 +12,7 @@ export default function Search() {
           </Link>
           <div className={styles['search-bar']}>
             <input
-              type='text'
+              type='search'
               name=''
               id=''
               placeholder='검색어를 입력해주세요'
@@ -24,20 +24,36 @@ export default function Search() {
             />
           </div>
         </div>
-        <div>
-          <div>
+        <div className={styles['search-history']}>
+          <div className={styles['search-history-header']}>
             <h3>최근 검색어</h3>
             <button className={styles['delete-history-button']}>
               <Trash2 />
             </button>
           </div>
-          <div>
-            <div className='word'>후드집업</div>
-            <div className='word'>바람막이</div>
-            <div className='word'>버뮤다 팬츠</div>
-            <div className='word'>가디건</div>
-            <div className='word'>마인드 브릿지</div>
+          <div className={styles['search-history-container']}>
+            <div className={styles['word']}>후드집업</div>
+            <div className={styles['word']}>바람막이</div>
+            <div className={styles['word']}>버뮤다 팬츠</div>
+            <div className={styles['word']}>가디건</div>
+            <div className={styles['word']}>마인드 브릿지</div>
           </div>
+        </div>
+        <hr height='3px' color='#E9E9E9' />
+        <div className={styles['search-rank']}>
+          <h3>인기 검색어</h3>
+          <ol>
+            <li>셔츠</li>
+            <li>가디건</li>
+            <li>아디다스</li>
+            <li>후드집업</li>
+            <li>바람막이</li>
+            <li>후드티</li>
+            <li>레이어드</li>
+            <li>뮬</li>
+            <li>데님 자켓</li>
+            <li>맨투맨</li>
+          </ol>
         </div>
       </div>
     </>
