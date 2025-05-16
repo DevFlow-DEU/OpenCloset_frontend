7666da24a4bc52964b0c92a52dd9bcf8be7ab7c9
yooningu <yig0717@naver.com>
2025-05-17 01:26:43 +0900

    로그인 페이지 개발
    거의 다 한듯

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,9 +2,14 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
 import Particular from './pages/Particular';
 import Search from './pages/Search';
+import Login from './pages/Login';
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/Home',
     element: <Home />,
   },
   {
@@ -15,6 +20,7 @@ const router = createBrowserRouter([
     path: '/search',
     element: <Search />,
   },
+ 
 ]);
 
 export default function Router() {
