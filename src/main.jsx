786ea44308a8997e7ca3bc7edf93f225a2e93ed8
jihongeek <jihongeek@kakao.com>
2025-06-09 22:52:20 +0900

    Chore(mock): 모킹을 위한 의존성 설치 및 설정, 코드 추가

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import Router from './routes';
+async function enableMocking() {
+  if (import.meta.env.VITE_MOCK !== 'true') {
+    return;
+  }
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Router />
-  </StrictMode>
-);
+  const { worker } = await import('./mocks/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <Router />
+    </StrictMode>
+  );
+});
