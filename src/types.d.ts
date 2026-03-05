566556c097c99f44dc57fd753a5a8c39207d65dd
jihongeek <jihongeek@kakao.com>
2026-03-05 16:06:24 +0900

    chore: 카카오 지도용 타입 추가

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,26 @@
+/// <reference types="vite/client" />
+
 declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
+}
+
+interface Window {
+  kakao?: {
+    maps?: {
+      load: (callback: () => void) => void;
+      LatLng: new (latitude: number, longitude: number) => unknown;
+      Map: new (
+        container: HTMLElement,
+        options: { center: unknown; level: number }
+      ) => {
+        setCenter: (latLng: unknown) => void;
+        relayout: () => void;
+      };
+      Marker: new (options: { map: unknown; position: unknown }) => {
+        setPosition: (latLng: unknown) => void;
+      };
+    };
+  };
 }
 
