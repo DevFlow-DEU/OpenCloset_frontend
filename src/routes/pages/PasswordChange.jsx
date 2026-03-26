94f121d37266ee1780511ced046698a104459afe
jihongeek <jihongeek@kakao.com>
2026-03-26 21:23:49 +0900

    fix: import 대소문자 수정

--- src/routes/pages/PasswordChange.jsx
+++ src/routes/pages/PasswordChange.jsx
@@ -1,109 +1,131 @@
 import { useEffect, useState } from 'react';
-import Header from '../../components/Header'
-import NavBar from '../../components/NavBar'
-import './share.css'
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { pwChangeSchema } from './PWChangeSchema.ts'
-import { useNavigate } from "react-router-dom";
-export default function PasswordChange(){
-
+import Header from '../../components/Header';
+import NavBar from '../../components/NavBar';
+import './share.css';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { pwChangeSchema } from './PWChangeSchema.ts';
+import { useNavigate } from 'react-router-dom';
+export default function PasswordChange() {
   const navigate = useNavigate();
-    // const [currentPassword,setcurrentPassword] = useState('')
-    // const [newPassword,setNewPassword] = useState('')
-    // const [checkPassword,setCheckPassword] = useState('')
-    const backUrl = import.meta.env.VITE_BACK_URL;
-  const [errorMSG,setErrorMSG] = useState('')
+  // const [currentPassword,setcurrentPassword] = useState('')
+  // const [newPassword,setNewPassword] = useState('')
+  // const [checkPassword,setCheckPassword] = useState('')
+  const backUrl = import.meta.env.VITE_BACK_URL;
+  const [errorMSG, setErrorMSG] = useState('');
   const {
-        register,
-        handleSubmit,
-        formState: { errors, isValid, isDirty},
-        reset,
-    } = useForm({
-        resolver: zodResolver(pwChangeSchema),
-        defaultValues: { currentPassword: "", newPassword: "",checkPassword:"" },
-        mode: "onChange",
-    });
+    register,
+    handleSubmit,
+    formState: { errors, isValid, isDirty },
+    reset,
+  } = useForm({
+    resolver: zodResolver(pwChangeSchema),
+    defaultValues: { currentPassword: '', newPassword: '', checkPassword: '' },
+    mode: 'onChange',
+  });
 
-
-    const onSubmit = async (values) => {
-      const { currentPassword, newPassword } = values;
-      const token = localStorage.getItem("token");
-   
+  const onSubmit = async (values) => {
+    const { currentPassword, newPassword } = values;
+    const token = localStorage.getItem('token');
 
     try {
       const res = await fetch(`${backUrl}/auth/password-change`, {
-        method: "PUT",
-        headers: { "content-type": "application/json",
-                   Authorization: `Bearer ${token}`,
-                  },
-         body: JSON.stringify({ currentPassword, newPassword }),
+        method: 'PUT',
+        headers: {
+          'content-type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ currentPassword, newPassword }),
       });
 
       const data = await res.json().catch(() => null);
 
-       if (res.ok) {
-      reset();
-      navigate("/");
-    } else {
-      setErrorMSG("비밀번호 변경 실패", data);
+      if (res.ok) {
+        reset();
+        navigate('/');
+      } else {
+        setErrorMSG('비밀번호 변경 실패', data);
+      }
+    } catch (error) {
+      setErrorMSG(error.message || '요청 중 오류가 발생했습니다.');
     }
-  } catch (error) {
-    setErrorMSG(error.message || "요청 중 오류가 발생했습니다.");
-  }
-};
+  };
 
+  return (
+    <>
+      <Header></Header>
+      <div className='SHcontainer'>
+        <form action='' onSubmit={handleSubmit(onSubmit)}>
+          <div className=' space-40px'></div>
 
-
-
-
-    return(
-        <>
-        
-        <Header></Header>
-        <div className='SHcontainer'>
-        <form action="" onSubmit={handleSubmit(onSubmit)}>
-            <div className=' space-40px'></div>
-
-            
-            <div>
+          <div>
             <p className='SHinput-tittle'>현재 비밀번호</p>
-            <input {...register("currentPassword")} className='SHinput' type="password" placeholder='현재 비밀번호 입력'/>
-            <div className={`SHinput-bar ${errors.currentPassword ? "red" : ""}  `}></div>
+            <input
+              {...register('currentPassword')}
+              className='SHinput'
+              type='password'
+              placeholder='현재 비밀번호 입력'
+            />
+            <div
+              className={`SHinput-bar ${errors.currentPassword ? 'red' : ''}  `}
+            ></div>
             <div className='SHinput-space space-40px'>
-               {errors.currentPassword  && <p className="SHinput-error">{errors.currentPassword.message}</p>}
-               
+              {errors.currentPassword && (
+                <p className='SHinput-error'>
+                  {errors.currentPassword.message}
+                </p>
+              )}
             </div>
-            </div>
+          </div>
 
           <div>
             <p className='SHinput-tittle'>새 비밀번호</p>
-            <input  {...register("newPassword")} className='SHinput' type="password" placeholder='새 비밀번호 입력'/>
-            <div className={`SHinput-bar  ${errors.newPassword ? "red" : ""}`} ></div>
+            <input
+              {...register('newPassword')}
+              className='SHinput'
+              type='password'
+              placeholder='새 비밀번호 입력'
+            />
+            <div
+              className={`SHinput-bar  ${errors.newPassword ? 'red' : ''}`}
+            ></div>
             <div className='SHinput-space space-28px'>
-               {errors.newPassword  && <p className="SHinput-error">{errors.newPassword.message}</p>}
+              {errors.newPassword && (
+                <p className='SHinput-error'>{errors.newPassword.message}</p>
+              )}
             </div>
-            </div>
+          </div>
 
-
-            <div>
+          <div>
             <p className='SHinput-tittle'>새 비밀번호 확인</p>
-            <input  {...register("checkPassword")} className='SHinput' type="password" placeholder='새 비밀번호 한번 더 입력'/>
-            <div className={`SHinput-bar ${errors.checkPassword ? "red" : ""}`} ></div>
+            <input
+              {...register('checkPassword')}
+              className='SHinput'
+              type='password'
+              placeholder='새 비밀번호 한번 더 입력'
+            />
+            <div
+              className={`SHinput-bar ${errors.checkPassword ? 'red' : ''}`}
+            ></div>
             <div className='SHinput-space space-28px'>
-               {errors.checkPassword  && <p className="SHinput-error">{errors.checkPassword.message}</p>}
+              {errors.checkPassword && (
+                <p className='SHinput-error'>{errors.checkPassword.message}</p>
+              )}
             </div>
-            </div>
-            
-             <button className={`SHsubmit ${isDirty && isValid ? "check" : ""}`} 
-                type='submit' 
-                disabled={!(isDirty && isValid)}> 제출하기 
-             </button>
-             <p className='SHinput-error errorMSG'>{errorMSG}</p>
-          
+          </div>
+
+          <button
+            className={`SHsubmit ${isDirty && isValid ? 'check' : ''}`}
+            type='submit'
+            disabled={!(isDirty && isValid)}
+          >
+            {' '}
+            제출하기
+          </button>
+          <p className='SHinput-error errorMSG'>{errorMSG}</p>
         </form>
-        </div>
-        <NavBar></NavBar>
-        </>
-    )
+      </div>
+      <NavBar></NavBar>
+    </>
+  );
 }
--- src/routes/pages/Registration.jsx
+++ src/routes/pages/Registration.jsx
@@ -1,21 +1,20 @@
-import { useState } from "react";
-import { FaCamera } from "react-icons/fa";
-import { Link, useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {RegistrationSchema } from "./RegistrationSchema";
-import { SlArrowDown } from "react-icons/sl";
-import Header from '../../components/Header'
-import NavBar from '../../components/NavBar'
-import "./Registration.css";
-import "./share.css";
-
+import { useState } from 'react';
+import { FaCamera } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { RegistrationSchema } from './RegistrationSchema';
+import { SlArrowDown } from 'react-icons/sl';
+import Header from '../../components/Header';
+import NavBar from '../../components/NavBar';
+import './Registration.css';
+import './share.css';
 
 export default function ProductRegistrationForm() {
   const backUrl = import.meta.env.VITE_BACK_URL;
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
   const navigate = useNavigate();
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState('');
 
   const {
     register,
@@ -26,20 +25,20 @@ export default function ProductRegistrationForm() {
   } = useForm({
     resolver: zodResolver(RegistrationSchema),
     defaultValues: {
-      title: "",
-      description: "",
-      price: "",
-      size: "사이즈 선택",
-      sex: "",
-      place: "",
-      date: "",
-      category: "카테고리 선택",
+      title: '',
+      description: '',
+      price: '',
+      size: '사이즈 선택',
+      sex: '',
+      place: '',
+      date: '',
+      category: '카테고리 선택',
       image: [],
     },
-    mode: "onChange",
+    mode: 'onChange',
   });
 
-  const image = watch("image");
+  const image = watch('image');
 
   const handleImageUpload = (e) => {
     if (!e.target.files) return;
@@ -50,12 +49,12 @@ export default function ProductRegistrationForm() {
       file,
     }));
 
-    setValue("image", [...image, ...newImages], { shouldValidate: true });
+    setValue('image', [...image, ...newImages], { shouldValidate: true });
   };
 
   const removeImage = (id) => {
     setValue(
-      "image",
+      'image',
       image.filter((item) => item.id !== id),
       { shouldValidate: true }
     );
@@ -64,48 +63,45 @@ export default function ProductRegistrationForm() {
   const onSubmit = async (values) => {
     try {
       const res = await fetch(`${backUrl}/board/create`, {
-        method: "POST",
+        method: 'POST',
         body: JSON.stringify({
-            ...values,
-            price: values.price.replace(/,/g, ""),
-          }),
+          ...values,
+          price: values.price.replace(/,/g, ''),
+        }),
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${token}`,
         },
       });
 
       if (res.ok) {
-        navigate("/home");
+        navigate('/home');
         return;
       }
 
-      setMessage("등록에 실패했습니다.");
+      setMessage('등록에 실패했습니다.');
     } catch (err) {
       console.log(err);
-      setMessage("서버에 연결할 수 없습니다.");
+      setMessage('서버에 연결할 수 없습니다.');
     }
   };
 
   return (
     <>
-    
-    <Header/>
-    <div className="SHcontainer">
-     
-
-      <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="space-40px"></div>
-          <p className="SHinput-tittle">사진 추가</p>
-          <div className="space-12px"></div>
-          <div className="image-container">
-            <label className="image-upload-button">
-              <div className="plus-icon">
+      <Header />
+      <div className='SHcontainer'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className='space-40px'></div>
+          <p className='SHinput-tittle'>사진 추가</p>
+          <div className='space-12px'></div>
+          <div className='image-container'>
+            <label className='image-upload-button'>
+              <div className='plus-icon'>
                 <FaCamera size={40} />
               </div>
               <input
-                type="file"
-                accept="image/*"
+                type='file'
+                accept='image/*'
                 onChange={handleImageUpload}
                 multiple
                 hidden
@@ -113,187 +109,192 @@ export default function ProductRegistrationForm() {
             </label>
 
             {image.map((item) => (
-              <div className="thumbnail" key={item.id}>
-                <img src={item.url} alt="상품 이미지" onClick={() => removeImage(item.id)}/>
+              <div className='thumbnail' key={item.id}>
+                <img
+                  src={item.url}
+                  alt='상품 이미지'
+                  onClick={() => removeImage(item.id)}
+                />
               </div>
             ))}
           </div>
-          <div className="SHinput-space space-28px">
+          <div className='SHinput-space space-28px'>
             {errors.image && (
-              <p className="SHinput-error">{errors.image.message}</p>
+              <p className='SHinput-error'>{errors.image.message}</p>
             )}
           </div>
-      
 
           <div>
-            <p className="SHinput-tittle">제목</p>
+            <p className='SHinput-tittle'>제목</p>
             <input
-              className="SHinput"
-              placeholder="상품명"
-              {...register("title")}
+              className='SHinput'
+              placeholder='상품명'
+              {...register('title')}
             />
-            <div className={`SHinput-bar ${errors.title ? "red" : ""}`}></div>
-            <div className="SHinput-space space-28px">
+            <div className={`SHinput-bar ${errors.title ? 'red' : ''}`}></div>
+            <div className='SHinput-space space-28px'>
               {errors.title && (
-                <p className="SHinput-error">{errors.title.message}</p>
+                <p className='SHinput-error'>{errors.title.message}</p>
               )}
             </div>
           </div>
 
           <div>
-            <p className="SHinput-tittle">가격</p>
-            <div className="select-wrapper">
-            <input
-              className="SHinput"
-              placeholder="상품 가격"
-              type="text"
-              value={watch("price")}
-              onChange={(e) => {
-                const formatted = e.target.value
-                  .replace(/[^0-9]/g, "")
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            <p className='SHinput-tittle'>가격</p>
+            <div className='select-wrapper'>
+              <input
+                className='SHinput'
+                placeholder='상품 가격'
+                type='text'
+                value={watch('price')}
+                onChange={(e) => {
+                  const formatted = e.target.value
+                    .replace(/[^0-9]/g, '')
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
-                setValue("price", formatted, {
-                  shouldDirty: true,
-                  shouldValidate: true,
-                });
-              }}
-            />
-            <span className="select-icon">원</span>
+                  setValue('price', formatted, {
+                    shouldDirty: true,
+                    shouldValidate: true,
+                  });
+                }}
+              />
+              <span className='select-icon'>원</span>
             </div>
-            <div className={`SHinput-bar ${errors.price ? "red" : ""}`}></div>
-            <div className="SHinput-space space-28px">
+            <div className={`SHinput-bar ${errors.price ? 'red' : ''}`}></div>
+            <div className='SHinput-space space-28px'>
               {errors.price && (
-                <p className="SHinput-error">{errors.price.message}</p>
+                <p className='SHinput-error'>{errors.price.message}</p>
               )}
             </div>
           </div>
 
           <div>
-            <p className="SHinput-tittle">판매 기간</p>
-            <div className="select-wrapper">
-            <input
-              className="SHinput"
-              type="number"
-              placeholder="판매 기간"
-              {...register("date")}
-            />
-            <span className="select-icon">일</span>
+            <p className='SHinput-tittle'>판매 기간</p>
+            <div className='select-wrapper'>
+              <input
+                className='SHinput'
+                type='number'
+                placeholder='판매 기간'
+                {...register('date')}
+              />
+              <span className='select-icon'>일</span>
             </div>
-            <div className={`SHinput-bar ${errors.date ? "red" : ""}`}></div>
-            <div className="SHinput-space space-28px">
+            <div className={`SHinput-bar ${errors.date ? 'red' : ''}`}></div>
+            <div className='SHinput-space space-28px'>
               {errors.date && (
-                <p className="SHinput-error">{errors.date.message}</p>
+                <p className='SHinput-error'>{errors.date.message}</p>
               )}
             </div>
           </div>
 
           <div>
-            <p className="SHinput-tittle">상세 정보</p>
+            <p className='SHinput-tittle'>상세 정보</p>
             <textarea
-              className="SHinput-200px"
-              placeholder="상세 내용"
-              {...register("description")}
+              className='SHinput-200px'
+              placeholder='상세 내용'
+              {...register('description')}
             />
             <div
-              className={`SHinput-bar ${errors.description ? "red" : ""}`}
+              className={`SHinput-bar ${errors.description ? 'red' : ''}`}
             ></div>
-            <div className="SHinput-space space-28px">
+            <div className='SHinput-space space-28px'>
               {errors.description && (
-                <p className="SHinput-error">{errors.description.message}</p>
+                <p className='SHinput-error'>{errors.description.message}</p>
               )}
             </div>
           </div>
 
           <div>
-            <p className="SHinput-tittle">대여 장소</p>
+            <p className='SHinput-tittle'>대여 장소</p>
             <input
-              className="SHinput"
-              placeholder="동으로 입력"
-              {...register("place")}
+              className='SHinput'
+              placeholder='동으로 입력'
+              {...register('place')}
             />
-            <div className={`SHinput-bar ${errors.place ? "red" : ""}`}></div>
-            <div className="SHinput-space space-28px">
+            <div className={`SHinput-bar ${errors.place ? 'red' : ''}`}></div>
+            <div className='SHinput-space space-28px'>
               {errors.place && (
-                <p className="SHinput-error">{errors.place.message}</p>
+                <p className='SHinput-error'>{errors.place.message}</p>
               )}
             </div>
           </div>
 
           <div>
-            <p className="SHinput-tittle">사이즈</p>
-            <div className="select-wrapper">
-            <select className="SHinput" {...register("size")}>
-              <option value="사이즈 선택">사이즈 선택</option>
-              <option value="S">S</option>
-              <option value="M">M</option>
-              <option value="L">L</option>
-              <option value="XL">XL</option>
-            </select>
-            <SlArrowDown size={24} className="select-icon"/>
+            <p className='SHinput-tittle'>사이즈</p>
+            <div className='select-wrapper'>
+              <select className='SHinput' {...register('size')}>
+                <option value='사이즈 선택'>사이즈 선택</option>
+                <option value='S'>S</option>
+                <option value='M'>M</option>
+                <option value='L'>L</option>
+                <option value='XL'>XL</option>
+              </select>
+              <SlArrowDown size={24} className='select-icon' />
             </div>
-            <div className={`SHinput-bar ${errors.size ? "red" : ""}`}></div>
-            <div className="SHinput-space space-28px">
+            <div className={`SHinput-bar ${errors.size ? 'red' : ''}`}></div>
+            <div className='SHinput-space space-28px'>
               {errors.size && (
-                <p className="SHinput-error">{errors.size.message}</p>
+                <p className='SHinput-error'>{errors.size.message}</p>
               )}
             </div>
           </div>
 
           <div>
-            <p className="SHinput-tittle">성별</p>
-            <div className="select-wrapper">
-            <select className="SHinput" {...register("sex")}>
-              <option value="">선택하기</option>
-              <option value="남성">남성</option>
-              <option value="여성">여성</option>
-            </select>
-            <SlArrowDown size={24} className="select-icon"/>
+            <p className='SHinput-tittle'>성별</p>
+            <div className='select-wrapper'>
+              <select className='SHinput' {...register('sex')}>
+                <option value=''>선택하기</option>
+                <option value='남성'>남성</option>
+                <option value='여성'>여성</option>
+              </select>
+              <SlArrowDown size={24} className='select-icon' />
             </div>
-            <div className={`SHinput-bar ${errors.sex ? "red" : ""}`}></div>
-            <div className="SHinput-space space-28px">
+            <div className={`SHinput-bar ${errors.sex ? 'red' : ''}`}></div>
+            <div className='SHinput-space space-28px'>
               {errors.sex && (
-                <p className="SHinput-error">{errors.sex.message}</p>
+                <p className='SHinput-error'>{errors.sex.message}</p>
               )}
             </div>
           </div>
 
           <div>
-            <p className="SHinput-tittle">카테고리</p>
-            <div className="select-wrapper">
-            <select className="SHinput" {...register("category")}>
-              <option value="카테고리 선택">카테고리 선택</option>
-              <option value="outer">아우터</option>
-              <option value="top">상의</option>
-              <option value="bottom">하의</option>
-              <option value="dress">원피스</option>
-              <option value="shoes">신발</option>
-              <option value="jewelry">주얼리</option>
-              <option value="bag">가방</option>
-              <option value="accessory">악세사리</option>
-            </select>
-            <SlArrowDown size={24} className="select-icon"/>
+            <p className='SHinput-tittle'>카테고리</p>
+            <div className='select-wrapper'>
+              <select className='SHinput' {...register('category')}>
+                <option value='카테고리 선택'>카테고리 선택</option>
+                <option value='outer'>아우터</option>
+                <option value='top'>상의</option>
+                <option value='bottom'>하의</option>
+                <option value='dress'>원피스</option>
+                <option value='shoes'>신발</option>
+                <option value='jewelry'>주얼리</option>
+                <option value='bag'>가방</option>
+                <option value='accessory'>악세사리</option>
+              </select>
+              <SlArrowDown size={24} className='select-icon' />
             </div>
-            <div className={`SHinput-bar ${errors.category ? "red" : ""}`}></div>
-            <div className="SHinput-space space-28px">
+            <div
+              className={`SHinput-bar ${errors.category ? 'red' : ''}`}
+            ></div>
+            <div className='SHinput-space space-28px'>
               {errors.category && (
-                <p className="SHinput-error">{errors.category.message}</p>
+                <p className='SHinput-error'>{errors.category.message}</p>
               )}
             </div>
           </div>
 
-        <button
-          className={`SHsubmit ${isDirty && isValid ? "check" : ""}`}
-          type="submit"
-          disabled={!(isDirty && isValid)}
-        >
-          등록하기
-        </button>
+          <button
+            className={`SHsubmit ${isDirty && isValid ? 'check' : ''}`}
+            type='submit'
+            disabled={!(isDirty && isValid)}
+          >
+            등록하기
+          </button>
 
-        {message && <p className="SHinput-error errorMSG">{message}</p>}
-      </form>
-    </div>
-    <NavBar/>
+          {message && <p className='SHinput-error errorMSG'>{message}</p>}
+        </form>
+      </div>
+      <NavBar />
     </>
   );
 }
