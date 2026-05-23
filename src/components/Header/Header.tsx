d03fbed69eb7f3cef9103347aef1ca77f3fbb422
jihongeek <jihongeek@kakao.com>
2026-05-23 14:15:39 +0900

    feat: 헤더 컴포넌트 합성 형식으로 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,49 +1,87 @@
 import styles from "./Header.module.css";
-import "../Fonts.css";
 import { Link } from "react-router-dom";
 
+import { type ReactNode } from "react";
 import ProfileIcon from "../../assets/header_profile.svg?react";
 import SearchIcon from "../../assets/header_search.svg?react";
 import BackIcon from "../../assets/header_back.svg?react";
-import Logo from "../../assets/header_logo.svg?react";
-interface Props {
-  title?: string;
-  hasNotch: boolean;
+import Logo from "../Logo/Logo";
+
+/*
+  버튼, 로고 분리요망
+*/
+
+interface RootProps {
+  children?: ReactNode;
+  hasNotch?: boolean;
 }
 
-export default function Header({ title }: Props) {
+function LinkGroup({ children }: { children: ReactNode }) {
+  return <span className={styles.linkGroup}>{children}</span>;
+}
+
+function Title({ title }: { title: string }) {
+  return (
+    <span className={styles.appBarCenter}>
+      <span className={styles.headerTitle}>{title}</span>
+    </span>
+  );
+}
+
+function BackButton() {
+  return (
+    <span>
+      <Link to={".."}>
+        <BackIcon />
+      </Link>
+    </span>
+  );
+}
+
+function SearchLink() {
+  return (
+    <span>
+      <Link to={"/Search"}>
+        <SearchIcon width={24} />
+      </Link>
+    </span>
+  );
+}
+
+function MyPageLink() {
+  return (
+    <span>
+      <Link to={"/MyPage"}>
+        <ProfileIcon width={24} />
+      </Link>
+    </span>
+  );
+}
+
+function Root({ hasNotch, children }: RootProps) {
   return (
     <>
       <div className={styles.headerBody}>
-        <div className={styles.appNotch}>
-          <div className={styles.camera}></div>
-        </div>
+        {hasNotch ? (
+          <div className={styles.appNotch}>
+            <div className={styles.camera}></div>
+          </div>
+        ) : null}
         {/* 노치 부분은 앱으로 만들면 없애야 함 */}
-        <div className={styles.appBar}>
-          <span>
-            <Link to={".."}>
-              <BackIcon />
-            </Link>
-          </span>
-          <span className={styles.appBarCenter}>
-            {title ? (
-              <span className={styles.headerTitle}>{title}</span>
-            ) : (
-              <Logo />
-            )}
-          </span>
-          <span>
-            <Link to={"/Search"}>
-              <SearchIcon width={24} />
-            </Link>
-            <Link to={"/MyPage"}>
-              <ProfileIcon width={24} />
-            </Link>
-          </span>
-        </div>
-        <div className={styles.headerBar}></div>
+        <div className={styles.appBar}>{children}</div>
+        <div className={styles.headerBar} />
       </div>
-      <div className={styles.headerSpace}></div>
     </>
   );
 }
+export default function Header({ children }: { children: ReactNode }) {
+  return children;
+}
+
+Header.Root = Root;
+Header.Title = Title;
+Header.MyPageLink = MyPageLink;
+Header.SearchLink = SearchLink;
+Header.BackButton = BackButton;
+Header.Logo = Logo;
+Header.LinkGroup = LinkGroup;
