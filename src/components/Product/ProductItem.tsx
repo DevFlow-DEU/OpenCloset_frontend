d2cbcfa06cfa3e7266c424339ab8a54cbf6b030c
jihongeek <jihongeek@kakao.com>
2026-06-04 15:02:27 +0900

    feat: 상품 설명에 링크 컴포넌트 추가 및 사진 무한 호출 방지 처리

--- src/components/Product/ProductItem.tsx
+++ src/components/Product/ProductItem.tsx
@@ -51,45 +51,49 @@ export default function ProductItem({
     <>
       <div className={styles.item}>
         <div className={styles.itemThumbnail}>
-          {wished ? (
-            <button className={styles.saveButton} onClick={onSaveButtonClick}>
+          <button className={styles.saveButton} onClick={onSaveButtonClick}>
+            {wished ? (
               <LikeRedHeartIcon width={"16"} height={"14px"} />
-            </button>
-          ) : (
-            <button className={styles.saveButton} onClick={onSaveButtonClick}>
+            ) : (
               <LikeGrayHeartIcon width={"16px"} height={"14px"} />
-            </button>
-          )}
+            )}
+          </button>
           <Link to={`/product/${id}`}>
             <img
               src={image ?? ItemNoImage}
               alt={String(id)}
-              onError={(error) => (error.currentTarget.src = ItemNoImage)}
+              onError={(error) => {
+                error.currentTarget.onerror = null;
+                error.currentTarget.src = ItemNoImage;
+              }}
             />
             {status === "예약중" || status === "대여중" ? (
               <div className={styles.statusLabel}>{status}</div>
             ) : null}
           </Link>
         </div>
-        <div className={styles.itemDescriptionContainer}>
-          <div className={styles.itemName}>{name}</div>
-          <div className={styles.costGroup}>
-            <span className={styles.itemCost}>
-              {rentalCost.toLocaleString("ko-kr", {
-                maximumFractionDigits: 4,
-              })}
-              원
-            </span>
-            <span className={styles.itemPeriod}> / {rentalPeriod}Day</span>
-          </div>
-          {startDate && parsedStartDate && endDate && parsedEndDate && (
-            <div className={styles.itemDate}>
-              {parsedStartDate.month}.{parsedStartDate.date}(
-              {getKoreanDay(startDate)}) ~ {parsedEndDate.month}.
-              {parsedEndDate.date}({getKoreanDay(endDate)})
+
+        <Link to={`/product/${id}`}>
+          <div className={styles.itemDescriptionContainer}>
+            <div className={styles.itemName}>{name}</div>
+            <div className={styles.costGroup}>
+              <span className={styles.itemCost}>
+                {rentalCost.toLocaleString("ko-kr", {
+                  maximumFractionDigits: 4,
+                })}
+                원
+              </span>
+              <span className={styles.itemPeriod}> / {rentalPeriod}Day</span>
             </div>
-          )}
-        </div>
+            {startDate && parsedStartDate && endDate && parsedEndDate && (
+              <div className={styles.itemDate}>
+                {parsedStartDate.month}.{parsedStartDate.date}(
+                {getKoreanDay(startDate)}) ~ {parsedEndDate.month}.
+                {parsedEndDate.date}({getKoreanDay(endDate)})
+              </div>
+            )}
+          </div>
+        </Link>
       </div>
     </>
   );
